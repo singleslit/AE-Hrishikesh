516ec94e072be2d988033e3763b8a836b7809edc
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-03-06 15:17:47 +0530

    Update VectorOfDigits.cpp

--- NumberOperation/VectorOfDigits.cpp
+++ NumberOperation/VectorOfDigits.cpp
@@ -1,8 +1,6 @@
 vector<ll> VOD(ll n) {
     string s = to_string(n);
-    vector<ll> digits(s.size()); // Allocate required space
-
+    vector<ll> digits(s.size());
     transform(s.begin(), s.end(), digits.begin(), [](char c) { return (c - '0'); });
-
     return digits;
 }
