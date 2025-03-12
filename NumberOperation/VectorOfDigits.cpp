33e94e005739702f772a33c24a3a635ed82dca63
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-03-12 17:53:59 +0530

    Delete NumberOperation directory

--- NumberOperation/VectorOfDigits.cpp
+++
@@ -1,6 +0,0 @@
-vector<ll> VOD(ll n) {
-    string s = to_string(n);
-    vector<ll> digits(s.size());
-    transform(s.begin(), s.end(), digits.begin(), [](char c) { return (c - '0'); });
-    return digits;
-}
