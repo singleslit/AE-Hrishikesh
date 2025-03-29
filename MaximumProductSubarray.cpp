d029db9af1c0d63faac26b6e845cb6d1b7a938bf
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-03-29 21:32:10 +0530

    Update MaximumProductSubarray.cpp

--- MaximumProductSubarray.cpp
+++ MaximumProductSubarray.cpp
@@ -1,46 +1,11 @@
-int MPS(const vll &a){
-    ll n1=-1, n2=-1, zeropos=-1, negseen=-1;
-    int128 ans = -INF, prod = 1;
-    vector<int128> pm(sz(a));
-    fori(i,0,sz(a)){
-        if (a[i] == 0){
-            n1 = -1; n2 = -1;
-            zeropos = i;
-            prod = 1;
-            pm[i] = 0;
-            ans = max(ans, pm[i]);
-        }
-        else if (a[i] < 0 && n1 == -1){
-            n1 = i;
-            negseen = 1;
-            prod *= a[i];
-            pm[i] = prod;
-            ans = max(ans, pm[i]);
-        }
-        else if (a[i] < 0 && n1 != -1 && n2 == -1){
-            n2 = i;
-            negseen = 2;
-            prod *= a[i];
-            pm[i] = prod;
-            ans = max(ans, pm[i]);
-        }
-        else if (a[i] < 0 && n1 != -1 && n2 != -1){
-            prod *= a[i];
-            pm[i] = prod;
-            negseen += 1;
-            if (isEven(negseen)){
-                if (n1 - 1 >= 0) ans = max(ans, pm[i] / pm[n1 - 1]);
-                else ans = max(ans, pm[i]);
-            } else {
-                if (n2 - 1 >= 0) ans = max(ans, pm[i] / pm[n2 - 1]);
-                else ans = max(ans, pm[i]);
-            }
-        }
-        else {
-            prod *= a[i];
-            pm[i] = prod;
-            ans = max(ans, pm[i]);
-        }
-    }
-    return ans;
+int MPS(vii A) {//Using 2P approach ; the most intuitive and best. It essentially creates suffix and prefix product to find the greatest prod
+  int n = A.size(), res = A[0], l = 0, r = 0;
+  fori(i,0,n) {
+    l =  (l ? l : 1) * A[i];
+    r =  (r ? r : 1) * A[n - 1 - i];
+    res = max(res, max(l, r));
+  }
+  return res;
 }
+
+
