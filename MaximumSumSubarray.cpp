e81776a4655ae287339f6a360e7e1d0b376e1d6d
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:40:37 +0530

    Update MaximumSumSubarray.cpp

--- MaximumSumSubarray.cpp
+++ MaximumSumSubarray.cpp
@@ -1,8 +1,8 @@
-int MSS(const vii &a){
-    int ans=0; int maxi=0;
-    fori(i,0,sz(a)){
+int MSS(const vii &a) {
+    int ans = 0, maxi = 0;
+    fi(sz(a)) {
         if (ans + a[i] >= 0) ans += a[i], maxi = max(maxi, ans);
         else ans = 0;
     }
     return maxi;
-} 
+}
