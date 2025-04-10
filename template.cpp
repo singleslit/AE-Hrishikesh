2e9d43414d9d634e12f36a9e382cc94b25ef113e
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:25:20 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -177,7 +177,18 @@ bool isPerfSquare(int num) {
 
     return x * x == num;
 }
-//
+//number to Binary String template
+template<typename T>
+string numToBinary(T n) {
+    if (n == 0) return "0";
+    string bin;
+    while (n) {
+        bin += (n & 1) ? '1' : '0';
+        n >>= 1;
+    }
+    reverse(all(bin));
+    return bin;
+}
 
 
 int main() {
