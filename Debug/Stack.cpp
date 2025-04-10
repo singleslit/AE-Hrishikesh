12a4fccbae0a2d9cef1c31aa7315852b7b73782c
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:54:33 +0530

    Delete Debug/Stack.cpp

--- Debug/Stack.cpp
+++
@@ -1,9 +0,0 @@
-template <typename T>
-void debugStack(std::stack<T> stk) {
-    std::cout << "Stack (top to bottom): ";
-    while (!stk.empty()) {
-        std::cout << stk.top() << " ";
-        stk.pop();
-    }
-    std::cout << "\n";
-}
