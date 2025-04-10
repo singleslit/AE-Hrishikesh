f446d8e7e84173eeb806a1382a76fd2b6037a316
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:53:44 +0530

    Delete MaximumSumSubarray.cpp

--- MaximumSumSubarray.cpp
+++
@@ -1,8 +0,0 @@
-int MSS(const vii &a) {
-    int ans = 0, maxi = 0;
-    fi(sz(a)) {
-        if (ans + a[i] >= 0) ans += a[i], maxi = max(maxi, ans);
-        else ans = 0;
-    }
-    return maxi;
-}
