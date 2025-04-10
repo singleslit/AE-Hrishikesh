9f4f8ca6bf962cca814722ac5f44fa3d02475c81
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:23:01 +0530

    Delete VectorOfDigits.cpp

--- VectorOfDigits.cpp
+++
@@ -1,7 +0,0 @@
-template <typename T>
-vii vod(T num) {
-    string s = str(num); // using str(x) macro
-    vii d; d.reserve(sz(s)); // sz(a) macro for size
-    felm(s) d.pb(elem - '0'); // felm(x) and pb macros
-    return d;
-}
