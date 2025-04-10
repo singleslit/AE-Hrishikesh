1e6924f3d920522f77b9d2743598a19c5db65943
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-04-10 11:04:11 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,12 +6,25 @@ const int MAX_N = 1e9;
 #define fori(i,a,b) for(int i=a;i<b;i++)
 #define rforl(i,a,b) for(ll i=a;i>b;i--)
 #define rfori(i,a,b) for(int i=a;i>b;i--)
+#define fi(n) fori(i,0,n)
+#define fj(n) fori(j,0,n)
+#define fk(n) fori(k,0,n)
 #define sz(a) a.size()
+#define felm(x) for(auto elem:x)
 #define pb push_back
 #define eb emplace_back
 #define lexi lexicographical_compare
 #define all(x) (x).begin(), (x).end()
 #define rall(x) (x).rbegin(), (x).rend()
+#define scan(x) cin >> x
+#define scan2(x, y) cin >> x >> y
+#define scanv(v) for (auto &e : v) cin >> e
+#define scanvv(vv) for (auto &v : vv) scanv(v)
+#define print(x) cout << (x) << '\n'
+#define printsp(x) cout << (x) << ' '
+#define print2(x, y) cout << (x) << ' ' << (y) << '\n'
+#define printv(v) for (auto &e : v) cout << e << ' '; cout << '\n'
+#define printvv(vv) for (auto &v : vv) printv(v)
 //sum macros
 #define suml(v) accumulate(all(v),0LL)
 #define sumi(v) accumulate(all(v),0)
@@ -25,7 +38,7 @@ const int MAX_N = 1e9;
 //vector macros
 #define slice(vec,start,end) vii((vec).begin() + (start), (vec).begin() + (end))
 #define vsert(v,idx, value) emplace(v.begin() + (idx), value)//for inserting an elem at an index in the vector
-#define VTS(v) string(all(v))
+#define vts(v) string(all(v))
 #define VOC(str) (std::vector<std::decay_t<decltype(str[0])>>((str).begin(), (str).end()))
 #define vjoin(v1, v2) (v1.insert(v1.end(), v2.begin(), v2.end()))
 #define join(v) std::accumulate((vec).begin(), (vec).end(), std::string(""))
@@ -37,17 +50,19 @@ const int MAX_N = 1e9;
 #define dsort(v) sort(all(v), greater<>())
 #define sortp2(v) sort((v).begin(), (v).end(), [](auto &a, auto &b) { return a.second < b.second; })
 #define dsortp2(v) sort((v).begin(), (v).end(), [](auto &a, auto &b) { return a.second > b.second; })
-#define debug(x) cerr << #x << " = " << x << endl;
 //math macros
 #define isEven(n) n%2==0
 #define isOdd(n) n%2==1
 #define manhdist(x1,y1,x2,y2) abs(x1-x2)+abs(y1-y2)
 #define COD(n) ((n) == 0 ? 1 : (int)log10(abs(n)) + 1)
 #define add(a,b) a=(a+(b))%MOD
-//lazy macros
+#define ceil2(a, b) (((a) + (b) - 1) / (b))
+//string macros
+#define str(x) to_string(x)
+//array macros
 #define cleantable(m,v) memset(m,v,sizeof(m));
-#define yes cout<<"Yes"
-#define no cout<<"No"
+//debug macros
+#define debug(x) cerr << #x << " = " << x << endl;
 using namespace std;
 using ll = long long;
 using vll = vector<ll>;
@@ -75,12 +90,11 @@ using stkint = stack<int>;
 using stkll = stack<ll>;
 using stkpii = stack<pii>;
 using stkpll = stack<pll>;
+#define INF LLONG_MAX
+#define NINF LLONG_MIN
 
-#define INF 1e9 
-
-#define INF 1e9 
- 
 int main() {
   trainingForICPC();
+ 
 }
 
