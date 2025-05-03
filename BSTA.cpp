bd426c8f823a3d2a09ed852b7b5423c9d5ce5bad
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-05-03 13:01:08 +0530

    Create BSTA.cpp

---
+++ BSTA.cpp
@@ -0,0 +1,9 @@
+template <typename F>
+ll bsta(F check, ll ok, ll ng, bool check_ok = true) {
+  if (check_ok) assert(check(ok));
+  while (abs(ok - ng) > 1) {
+    ll x = (ok + ng) / 2;
+    (check(x) ? ok : ng) = x;
+  }
+  return ok;
+}
