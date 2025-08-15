0dfeeecac330a15c34a3b51ffeb62a9ffbfe23c1
Hrishikesh Vinod <h.kesh.vinod@gmail.com>
2025-08-15 17:15:12 +0530

    Update DisjointSetUnion.cpp
    added largest component size and component count

--- DisjointSetUnion.cpp
+++ DisjointSetUnion.cpp
@@ -1,20 +1,25 @@
 struct dsu
 {
-  vii parent, size;
-  dsu(int n)
+  vll parent, size;
+  ll component_count;
+  ll largest_component;
+  
+  dsu(ll n)
   {
     parent.resize(n);
     size.assign(n, 1);
     rep(i,n) parent[i] = i;
+    component_count=n-1;//for 1th based indexing graphs
+    largest_component=1;
   }
 
-  int leader(int x)
+  ll leader(ll x)
   {
     if (parent[x] == x) return x;
     return parent[x] = leader(parent[x]);
   }
 
-  bool merge(int x, int y)
+  bool merge(ll x, ll y)
   {
     x = leader(x);
     y = leader(y);
@@ -22,16 +27,19 @@ struct dsu
     if (size[x] < size[y]) swap(x, y);
     parent[y] = x;
     size[x] += size[y];
+    largest_component=max(largest_component,size[x]);
+    component_count--;
     return true;
   }
 
-  bool same(int x, int y)
+  bool same(ll x, ll y)
   {
     return (leader(x) == leader(y));
   }
 
-  int setsz(int x)
+  ll setsz(ll x)
   {
     return size[leader(x)];
   }
+  
 };
